6f53152af6d961042e7e09d408b18452517c4a27
jerry <309032663@qq.com>
2019-08-21 15:43:54 +0800

    update

--- app/src/main/java/com/cxyzy/demo/utils/Constants.kt
+++ app/src/main/java/com/cxyzy/demo/utils/Constants.kt
@@ -11,7 +11,7 @@ object ApiToken {
 }
 
 object OkHttpUrl {
-    const val BASE_URL = "https://www.tianqiapi.com/networkApi/"
+    const val BASE_URL = "https://www.tianqiapi.com/api/"
     const val LOGIN_URL = "user/login"
 }
 
--- level_2/src/main/java/com/cxyzy/demo/utils/Constants.kt
+++ level_2/src/main/java/com/cxyzy/demo/utils/Constants.kt
@@ -7,6 +7,6 @@ object ApiToken {
 }
 
 object OkHttpUrl {
-    const val BASE_URL = "https://www.tianqiapi.com/networkApi/"
+    const val BASE_URL = "https://www.tianqiapi.com/api/"
     const val LOGIN_URL = "user/login"
 }
