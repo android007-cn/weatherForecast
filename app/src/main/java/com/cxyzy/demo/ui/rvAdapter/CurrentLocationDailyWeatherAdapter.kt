3f337bd48e2d8650521f5f2aa145b21fb6d1b849
jerry <309032663@qq.com>
2019-08-21 17:02:19 +0800

    update

--- app/src/main/java/com/cxyzy/demo/ui/rvAdapter/CurrentLocationDailyWeatherAdapter.kt
+++ app/src/main/java/com/cxyzy/demo/ui/rvAdapter/CurrentLocationDailyWeatherAdapter.kt
@@ -1,14 +1,22 @@
+/*
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package com.cxyzy.demo.ui.rvAdapter
 
 import android.text.TextUtils
 import com.afollestad.assent.Permission
 import com.afollestad.assent.runWithPermissions
-import com.cxyzy.demo.R
 import com.cxyzy.demo.utils.*
 import com.cxyzy.utils.LocationUtils
 import com.cxyzy.utils.ext.toast
 
 class CurrentLocationDailyWeatherAdapter(locationId: String) : BaseDailyWeatherAdapter(locationId) {
+
     private fun locateAndFetchWeatherRequirePermission() {
         activity.runWithPermissions(Permission.ACCESS_FINE_LOCATION) {
             if (LocationUtils.isLocationProviderEnabled(activity)) {
