caedd41b4f777a6002308cf7c30fc696c6428e3a
jerry <309032663@qq.com>
2019-10-22 15:19:52 +0800

    update

--- level_2/src/main/java/com/cxyzy/weather/network/HttpRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.cxyzy.weather.network
-
-object HttpRepository : BaseHttpRepository() {
-    suspend fun getDailyWeather() = networkApi.getDailyWeather(cityName = "南京")
-}
--- level_2/src/main/java/com/cxyzy/weather/network/request/Requests.kt
+++
@@ -1,2 +0,0 @@
-package com.cxyzy.weather.network.request
-
